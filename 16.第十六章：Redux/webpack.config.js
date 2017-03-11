4ed507922bb55036a29b10e322c8828e00e0c6c8
mar-er <1051221329@qq.com>
2017-03-11 10:05:01 +0800

    update redux

--- 16.第十六章：Redux/webpack.config.js
+++ 16.第十六章：Redux/webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 var path = require('path');
 module.exports = {
     entry:{
-        build1:"./22.1.React redux让UI和Store相配合没使用react-redux插件.js",
+        build:"./21.React-redux让UI和Store相配合.js",
     },
     output:{
         path:"./",
